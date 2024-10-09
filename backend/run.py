25452d64e0b189ead09a15891888e5ec722e09e3
Aditya Chandran <adityachandrn1@gmail.com>
2024-10-09 20:55:51 +0530

    Add run.py for more control over server process

---
+++ backend/run.py
@@ -0,0 +1,14 @@
+import uvicorn
+from dotenv import load_dotenv
+from os import getenv, path
+import sys
+
+if __name__ == "__main__":
+    load_dotenv()
+    sys.path.append(path.join(path.dirname(__file__), "src"))
+
+    SERVER_HOST = getenv("SERVER_HOST")
+    SERVER_PORT = int(getenv("SERVER_PORT"))
+    DEV_ENV = bool(getenv("DEV_ENV"))
+    uvicorn.run("fastapi_server:app", host=SERVER_HOST,
+                port=SERVER_PORT, reload=DEV_ENV)
